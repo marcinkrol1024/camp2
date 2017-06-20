4bc65aac1515e64bae1ee8c4ab5bc4bc7fdb8957
Marcin Król <marcins1024@gmail.com>
2017-06-20 16:16:24 +0200

    Handle exceptions

--- src/main/java/sages/bootcamp/names/NamesApp.java
+++ src/main/java/sages/bootcamp/names/NamesApp.java
@@ -10,15 +10,25 @@ public class NamesApp {
 
   private final static Logger log = LoggerFactory.getLogger(NamesApp.class);
 
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) {
     log.debug("Starting names app");
 
     NamesReader namesReader = new NamesReader();
     NamesJoiner namesJoiner = new NamesJoiner();
-    NamesProperties namesProperties = new NamesPropertiesFactory()
-        .createNamesProperties("application.properties");
+    NamesProperties namesProperties = null;
+    try {
+      namesProperties = new NamesPropertiesFactory()
+          .createNamesProperties("application.properties");
+    } catch (IOException e) {
+      log.error("Failed to load properties file.", e);
+    }
 
-    List<String> names = namesReader.read(namesProperties.getNamesFile());
+    List<String> names = null;
+    try {
+      names = namesReader.read(namesProperties.getNamesFile());
+    } catch (IOException e) {
+      log.error("Failed to load names file.", e);
+    }
     String joinedNames = namesJoiner.join(names, namesProperties.getNamesSeparator());
 
     // wypisywanie
