1e7025c05c11137c17326fe8757a788bd94d1ad2
Marcin Król <marcins1024@gmail.com>
2017-06-20 15:03:32 +0200

    Initial refactoring

--- src/main/java/sages/bootcamp/names/NamesApp.java
+++ src/main/java/sages/bootcamp/names/NamesApp.java
@@ -3,19 +3,31 @@ package sages.bootcamp.names;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class NamesApp {
   public static void main(String[] args) throws IOException {
     // try-with-resources
     try (BufferedReader namesReader = new BufferedReader(new FileReader("names"))) {
+
+      // wczytywanie imion
+      List<String> names = new ArrayList<>();
       String name;
-      StringBuilder namesBuilder = new StringBuilder();
-      if ((name = namesReader.readLine()) != null) {
-        namesBuilder.append(name);
-      }
       while ((name = namesReader.readLine()) != null) {
-        namesBuilder.append(", ").append(name);
+        names.add(name);
       }
+
+      // budowanie stringa z imion
+      StringBuilder namesBuilder = new StringBuilder();
+      if (names.size() > 1) {
+        namesBuilder.append(names.get(0));
+        for (int i = 1; i < names.size(); i++) {
+          namesBuilder.append(", ").append(names.get(i));
+        }
+      }
+
+      // wypisywanie
       System.out.println(namesBuilder.toString());
     }
   }
