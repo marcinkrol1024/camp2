894cb53cad1b7ea641334bb41b40a57e92a35138
Marcin Król <marcins1024@gmail.com>
2017-06-20 15:36:19 +0200

    Read namesFile from properties

--- src/main/java/sages/bootcamp/names/NamesApp.java
+++ src/main/java/sages/bootcamp/names/NamesApp.java
@@ -1,14 +1,25 @@
 package sages.bootcamp.names;
 
+import com.google.common.io.Resources;
+
 import java.io.IOException;
 import java.util.List;
+import java.util.Properties;
 
 public class NamesApp {
   public static void main(String[] args) throws IOException {
+    Properties properties = new Properties();
+    properties.load(
+        Resources
+            .getResource("application.properties")
+            .openStream()
+    );
+    String namesFiles = properties.getProperty("names.files.path");
+
     NamesReader namesReader = new NamesReader();
     NamesJoiner namesJoiner = new NamesJoiner();
 
-    List<String> names = namesReader.read("names");
+    List<String> names = namesReader.read(namesFiles);
     String joinedNames = namesJoiner.join(names);
 
     // wypisywanie
